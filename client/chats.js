71fddac0fd8df4467f3b6da9c569db71499747c1
Stephen Shepherd <slshephe@ucsd.edu>
2016-02-11 23:10:32 -0800

    finish the thing

---
+++ client/chats.js
@@ -0,0 +1,39 @@
+Template.chats.helpers({
+    chats: [
+	    	{
+    		handshake: true,
+    		pageUrl: "/chat/mysterymeet",
+			myFoodImg: "images/support-logo.JPG",
+			partnerFoodImg: "images/support-logo.JPG",
+            name: "Mystery Meet Support"
+    	},
+    	{
+    		handshake: false,
+    		pageUrl: "/chat/user1",
+			myFoodImg: "images/food1.jpg",
+			partnerFoodImg: "images/food2.jpg",
+            name: "User1"
+    	},
+    	{	
+    		handshake: false,
+    		pageUrl: "/chat/user2",
+			myFoodImg: "images/food1.jpg",
+			partnerFoodImg: "images/food3.jpg",
+		    name: "User2"
+    	},
+    	{
+    		handshake: false,
+    		pageUrl: "/chat/user3",
+			myFoodImg: "images/food1.jpg",
+			partnerFoodImg: "images/food5.jpg",
+			name: "User3"
+    	},
+    	{
+    		handshake: true,
+    		pageUrl: "/chat/user4",
+			myFoodImg: "images/food3.jpg",
+			partnerFoodImg: "images/food4.jpg",
+			name: "User4"
+    	}
+    ]
+  });
