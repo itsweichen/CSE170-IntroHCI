8ceea500ad850edd16bb46eda6dbadae2ea49a85
Mitchell Wulfman <mitchwulfman@gmail.com>
2016-02-11 22:47:59 -0800

    make logout work

--- client/profile.js
+++ client/profile.js
@@ -1,5 +1,11 @@
-
-
 Template.profile.helpers({
     profileUrl: "http://graph.facebook.com/" + Meteor.user().services.facebook.id + "/picture/?type=large"
+});
+
+Template.profile.events({
+    'click #logout': function(){
+        console.log('logout')
+        Accounts.logout()
+        return FlowRouter.go('login')
+    }
 });
