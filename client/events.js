02bbf00f30e69e011fefae0bfd3a3f08494eede9
Weichen Liu <kevbtc@gmail.com>
2016-02-11 18:47:57 -0800

    read events from JSON

---
+++ client/events.js
@@ -0,0 +1,44 @@
+Template.events.helpers({
+    events: [
+    	{
+    		isUpcoming: true,
+    		pageUrl: "/drop",
+			myFoodImg: "images/food1.jpg",
+			partnerFoodImg: "images/food2.jpg",
+			time: "1:00pm 02/15/2016",
+			location: "UCSD",
+			labelType: "label label-primary",
+			labelContent: "Waiting to drop"
+    	},
+    	{	
+    		isUpcoming: true,
+    		pageUrl: "/find-your-meal",
+			myFoodImg: "images/food1.jpg",
+			partnerFoodImg: "images/food3.jpg",
+			time: "1:30pm 02/18/2016",
+			location: "UCSD",
+			labelType: "label label-primary",
+			labelContent: "Find your meal"
+    	},
+    	{
+    		isUpcoming: true,
+    		pageUrl: "/drop-wait",
+			myFoodImg: "images/food1.jpg",
+			partnerFoodImg: "images/food5.jpg",
+			time: "1:30pm 02/18/2016",
+			location: "UCSD",
+			labelType: "label label-success",
+			labelContent: "You dropped. Waiting for other"	
+    	},
+    	{
+    		isUpcoming: false,
+    		pageUrl: "/finish",
+			myFoodImg: "images/food3.jpg",
+			partnerFoodImg: "images/food4.jpg",
+			time: "1:00pm 02/15/2016",
+			location: "UCSD",
+			labelType: "label label-default",
+			labelContent: "Finished"	
+    	}
+    ]
+  });
