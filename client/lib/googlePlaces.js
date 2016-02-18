08927e0c8be05b923140da85d9af4112e5e3c8cd
Stephen Shepherd <slshephe@ucsd.edu>
2016-02-18 14:24:24 -0800

    mrt maps commit

---
+++ client/lib/googlePlaces.js
@@ -0,0 +1,6 @@
+GoogleMaps.init({
+  'sensor': true, //optional
+  'key': 'AIzaSyDSj_bx-4ITRBN8iOd6uSfSqF80KO0LAf4', //optional
+  'language': 'en',  //optional
+  'libraries': 'places'
+});
--- client/post-meal.js
+++ client/post-meal.js
@@ -16,3 +16,20 @@ Template.postMeal.events({
         event.target.name.value = "";
     }
 });
+
+Template.postMeal.rendered = function () { 
+    window.onload = function() { 
+
+        input = document.getElementById('autocomplete'); 
+        autocomplete = new google.maps.places.Autocomplete(input); 
+
+        // When the user selects an address from the dropdown, 
+        google.maps.event.addListener(autocomplete, 'place_changed', function() { 
+
+             // Get the place details from the autocomplete object. 
+             var place = autocomplete.getPlace(); 
+
+             console.log("place: " + JSON.stringify(place) ); 
+        }); 
+    }; 
+};
