d83c3e523fcaf70c615eb6c2b09b230761cc90c3
Mitchell Wulfman <mitchwulfman@gmail.com>
2016-02-18 13:29:14 -0800

    insert meal  name and time

--- client/post-meal.js
+++ client/post-meal.js
@@ -2,17 +2,21 @@ Template.postMeal.events({
     "submit #post-meal-form": function (event) {
         // Prevent default browser form submit
         event.preventDefault();
-        console.log("fired")
 
         // Get value from form element
         var name = event.target.mealName.value;
+        var time = event.target.time.value;
 
         // Insert a task into the collection
         Meals.insert({
-            name: name
+            //image: image,
+            name: name,
+            time: time,
+            //location: location,
         });
 
         // Clear form
-        event.target.name.value = "";
+        event.target.mealName.value = "";
+        event.target.time.value = "";
     }
 });
