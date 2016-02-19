f560559556950dcf13015d3a79310af7cb11441e
Weichen Liu <kevbtc@gmail.com>
2016-02-18 17:38:06 -0800

    add structure for events

--- client/timer.js
+++ client/timer.js
@@ -10,5 +10,26 @@ function createMatch()
   		console.log("Timer Interrupt." + "count: " + count + " timerIntervel:" + timerIntervel);
 
   		// match two meal, create an event and add it to db
+
+  		/*
+
+		Events.insert({
+            image1: imageUrl,
+            name1: name,
+            time1: time,
+            location1: location,
+            user1: loggedInUserId,
+
+            image2: imageUrl,
+            name2: name,
+            time2: time,
+            location2: location,
+            user2: loggedInUserId,
+
+            status: 1// 1: waiting to drop; 2: Find your meal; 3: Waiting for other to drop; 4: Finished
+
+		});
+
+  		*/
   	}
 }
