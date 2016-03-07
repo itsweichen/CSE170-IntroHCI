8ad4de150a7c46934ae8cf0e97594ddc555d2632
Weichen Liu <kevbtc@gmail.com>
2016-03-07 14:03:37 -0800

    fix random img url bugs

--- client/browse-meals.js
+++ client/browse-meals.js
@@ -1,9 +1,9 @@
 Template.browseMeals.events({
     'click #swipeRight': function(){
     	// create an event and go to that event
-        var randomImgIndex = Math.floor(Math.random() * 5);
+        var randomImgIndex = Math.floor(Math.random() * 5) + 1;
         var img1 = '/images/food'+randomImgIndex+'.jpg';
-        var randomImgIndex2 = randomImgIndex + 1;
+        var randomImgIndex2 = (randomImgIndex + Math.floor(Math.random() * 5)) % 5 + 1;
         var img2 = '/images/food'+randomImgIndex2+'.jpg';
     	Events.insert({
     		    user1: {
